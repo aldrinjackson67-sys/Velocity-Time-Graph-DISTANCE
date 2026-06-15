325c3b1a0d2ecbbf0a774bb67b27e36d498e7b0e
aldrinjackson67-sys <aldrinjackson67@gmail.com>
2026-06-15 15:06:52 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,49 +2,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# ==========================================
 # ENTER TIME VALUES (seconds)
-# ==========================================
 
-time = [0, 120, 240, 360, 480, 600, 720, 840, 960, 1080, 1200]
+time = [0, 60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 660, 720, 780, 840, 900, 960, 1020, 1080, 1140, 1200]
 
-# ==========================================
+
 # ENTER SPEED VALUES (km/h)
-# Must match time values
-# ==========================================
 
-speed_kmh = [0, 40, 18, 25, 21, 34, 36, 68, 67, 49, 0]
+speed_kmh = [0, 48, 43, 51, 18, 50, 28, 33, 21, 62, 34, 32, 36, 52, 68, 67, 67, 39, 49, 20, 0]
 
-# ==========================================
+
 # ACTUAL DISTANCE FROM GOOGLE MAPS
 # 16 km = 16000 meters
-# ==========================================
+
 
 actual_distance = 16000
 
-# ==========================================
 # CONVERT km/h TO m/s
-# ==========================================
 
 speed_ms = []
 
 for s in speed_kmh:
     speed_ms.append(s * 5 / 18)
 
-# ==========================================
 # CALCULATE DISTANCE USING GRAPH
-# ==========================================
 
 distance = np.trapezoid(speed_ms, time)
-# ==========================================
 # PERCENTAGE ERROR
-# ==========================================
 
 percentage_error = abs(distance - actual_distance) / actual_distance * 100
 
-# ==========================================
 # RESULTS
-# ==========================================
 
 print("\n========== RESULTS ==========")
 
@@ -54,9 +42,7 @@ print(f"Actual Distance     = {actual_distance:.2f} meters")
 
 print(f"Percentage Error    = {percentage_error:.2f} %")
 
-# ==========================================
 # PLOT GRAPH
-# ==========================================
 
 plt.figure(figsize=(10,5))
 
